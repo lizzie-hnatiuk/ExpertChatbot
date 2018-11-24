db076c95679e6f8e6b37a11b3ad94a9cdeb86782
lizzie-hnatiuk <lizzie.hnatiuk@gmail.com>
2018-11-23 16:10:13 -0800

    main almost working (bugs: - user input must be within quotes or a single word, - must type . to get a response)

--- ChatBot.pl
+++ ChatBot.pl
@@ -1,4 +1,5 @@
 % Pronoun Reversal
+% knowledge base portion of program
 % changes grammatical person of sentance (for response creation)
 me_you(me, you).
 me_you(i, you).
@@ -10,12 +11,14 @@ me_you(am, are).
 % takes an input string of user text
 % returns a response string
 main :-
-  write('Expert ChatBot Sample'),
+  write("Expert ChatBot Sample"), nl,
   % repeat forces code to endlessly repreat (until something termiates backtracking)
+  write("Ask me anything!"), nl,
   repeat,
-  write('➟ '), read_string(InputString),
-  respond(InputString, ResponseString),
-  write(ResponseString),
+  write("➟ "), nl,
+  read(InputString),
+  respond(InputString, ResponseString), nl,
+  write(ResponseString), nl,
   % forces backtracking until user inputs 'quit'
   InputString == 'quit'.
 
@@ -41,3 +44,25 @@ respond(InputString, ResponseString) :-
     string_concat(Word, " ", WordWithSpace),
     list_to_string(T, TailString),
     string_concat(WordWithSpace, TailString, String).
+
+%Using Pronoun Reversal to Create Response
+%boundary condition: if list is empty, were done.
+swap_person([], []).
+%recursive condition:
+%    - take first ward in list and call swap_word/2
+%    - put replacement word in output list
+%    - recursively call rest of input list
+swap_person([X|Xs], [Y|Ys]) :-
+  swap_word(X, Y),
+  !, swap_person(Xs, Ys).
+
+%looks at K.B. to see if word is mentioned in pronoun reversals
+swap_word(X, Y) :- me_you(X, Y).
+swap_word(X, Y) :- me_you(Y, X).
+%if not in K.B., word is replaced with itself
+swap_word(W,W).
+
+%Forming the Resopnse
+% for now, simply add question mark to end
+form_response(In, Out) :-
+  append(In, ["?"], Out).
