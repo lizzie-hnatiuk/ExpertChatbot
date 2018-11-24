9e7d15353281e1a4dbb29e7ba762b99cf81a9514
lizzie-hnatiuk <lizzie.hnatiuk@gmail.com>
2018-11-23 21:02:46 -0800

    Grneral response rules added (for conversation flow) (bug: response patterns only work for a single word in front of key word e.g.: response pattern for [_,"want","to",X] is called for input "i want to do something" but not "i know i want to do something"

--- ChatBot.pl
+++ ChatBot.pl
@@ -1,5 +1,6 @@
+% Knowledge Base portion of program -----------------------------------------------
+
 % Pronoun Reversal
-% knowledge base portion of program
 % changes grammatical person of sentance (for response creation)
 me_you("me", "you").
 me_you("i", "you").
@@ -8,6 +9,22 @@ me_you("mine", "yours").
 me_you("am", "are").
 me_you("im", "you're").
 me_you("im", "you're").
+
+%general response rules for converation flow
+response( ["yes",_],
+    [ ["why",?] ]).
+response( ["no",_],
+    [ ["why","not",?] ]).
+response( [_,"you","like",X],
+    [ ["how","can","you","like",X,?],
+      ["is","it","strange","to","like",X,?] ]).
+response( [_,"want","to",X],
+          [ ["why","would","you","want","to",X,?],
+            ["you","can","not",X,'.'],
+            ["is","it","dangerous","to",X,?] ]).
+response( [X],
+          [ [X,?] ]).
+%Rule portion of program ----------------------------------------------------------
 
 % Input/Output
 % takes an input string of user text
@@ -36,18 +53,18 @@ respond(InputString, ResponseString) :-
   %- so we have one response per user input
   !.
 
-  %String/List conversion
-  %Split string into list of words sepearated by " "
-  string_to_list(String, WordList) :-
-    split_string(String, " ", "", WordList).
-  %Condense list of words into single String in same order
-  list_to_string([], "").
-  list_to_string([Word], String) :-
-    Word == String.
-  list_to_string([Word|T], String) :-
-    string_concat(Word, " ", WordWithSpace),
-    list_to_string(T, TailString),
-    string_concat(WordWithSpace, TailString, String).
+%String/List conversion
+%Split string into list of words sepearated by " "
+string_to_list(String, WordList) :-
+  split_string(String, " ", "", WordList).
+%Condense list of words into single String in same order
+list_to_string([], "").
+list_to_string([Word], String) :-
+  Word == String.
+list_to_string([Word|T], String) :-
+  string_concat(Word, " ", WordWithSpace),
+  list_to_string(T, TailString),
+  string_concat(WordWithSpace, TailString, String).
 
 %Using Pronoun Reversal to Create Response
 %boundary condition: if list is empty, were done.
@@ -66,7 +83,65 @@ swap_word(X, Y) :- me_you(Y, X).
 %if not in K.B., word is replaced with itself
 swap_word(W,W).
 
-%Forming the Resopnse
+%Forming the Response
 % for now, simply add question mark to end
+% remember input list has swapped pronouns!!!
 form_response(In, Out) :-
-  append(In, ["?"], Out).
+  response(InputPattern, ResponsePatterns),
+  match(InputPattern, In),
+  %random_elem(ResponsePatterns, ResponsePattern),
+  flatten(ResponsePatterns, Out).
+
+
+%returns random answer from list of possible answers
+random_elem(List, Elem) :-
+  count(List, N),
+  !,
+  random(1, N, I),
+  nth1(I, List, Elem).
+
+%counts number of elements in a list
+%count([], 0).
+count([_|Tail], N) :-
+  count(Tail, N1),
+  N is N1 + 1.
+
+%Matching Input with Response Rules
+% boundary condtions:
+%   - when input list is empty
+match([], []).
+%   - when only element left is a variable
+%        - variable bound to whatever is left of input
+match([Xs], IWs) :-
+    var(Xs),
+    !,
+    Xs = IWs.
+% recursive cases:
+%    - next element in input is a variable
+%        - call fill_var to gather words up
+%          until next specific word is encountered
+match([Xs,W|PWs], IWs) :-
+    var(Xs),
+    !,
+    fill_var(Xs, W, IWs, IWsLeft),
+    match([W|PWs], IWsLeft).
+%    - next element is a specific word
+%        - if matches user input, continue recursion
+match([W|PWs], [W|IWs]) :-
+    !,
+    match(PWs, IWs).
+
+%Walking Input List to Find Words
+% 4 arguments:
+%    - the output list of words
+%         - returned to caller
+%    - the next word in the input pattern
+%         - causes recursion to stop when it’s found
+%    - the user input list being walked
+%         - looking for the stop word
+%    - the remainder of the input list
+%         - returned to caller for further processing
+fill_var([], W, [W|IWs], [W|IWs]) :-
+    !.
+fill_var([X|Xs], W, [X|IWs], IWsLeft) :-
+    fill_var(Xs, W, IWs, IWsLeft).
