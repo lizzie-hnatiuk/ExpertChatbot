eb3b02d3158c462f2b81fd39ba7a31d524b46db4
lizzie-hnatiuk <lizzie.hnatiuk@gmail.com>
2018-11-24 20:17:11 -0800

    Added Basic Ontlolgy of Common Words * Ontology usable in Grammar Rules * Grammar Rules not usable by ChatBot yet

--- Grammar.pl
+++ Grammar.pl
@@ -1,6 +1,8 @@
 % *************** Grammar.pl contains the Relations for the ChatBot ***************
 % *********************  pertaining to general grammar rules *********************
 
+:- consult('ConversationOntology.pl').
+
 % ************************** Genrative Rules for Syntax **************************
 %   * S —> NP VP
 %   * NP —> (DET) (ADJ) N (PP)
@@ -15,11 +17,11 @@ sentence(L0,L2) :-
   verb_phrase(L1,L2).
 
 % topicalization: optional movement NP after verb in VP to front of sentance
-sentance(L0,L4) :-
-  noun_phrase(L0,L1),
-  noun_phrase(L1,L2),
-  verb(L2,L3),
-  pp(L3,L4).
+% sentance(L0,L4) :-
+%   noun_phrase(L0,L1),
+%   noun_phrase(L1,L2),
+%   verb(L2,L3),
+%   pp(L3,L4).
 
 % NounPhrase --> (Determiner) (Adjective) Noun (PrepositionalPhrase)
 noun_phrase(L0,L4) :-
@@ -39,32 +41,58 @@ pp(L0,L2) :-
   preposition(L0,L1),
   opt_noun_phrase(L1,L2).
 
-% ************************** Helpers for Generative Rules **************************
+% ************************** Helper Functions for Rules **************************
 
-% dictionary for determiners
-det(L,L).
-det(["a"|L],L).
-det(["the"|L],L).
-
-% adjectives is a sequence of adjectives adjectives(L,L).
+% Adjectives is a sequence of adjectives adjectives(L,L).
 adjectives(L0,L2) :-
-    adj(L0,L1),
-    adjectives(L1,L2).
+  adj(L0,L1),
+  adjectives(L1,L2).
+adjectives(L0,L1) :- adj(L0,L1).
 
 % optional determiners, adjectives, prepositional phrases, noun phrases
 %  are either nothing or themself
-opt_det(L,L).
 opt_det(L0,L1) :-
-   det(L0,L1).
+  det(L0,L1).
+opt_det(L,L).
 
-opt_adjectives(L,L).
 opt_adjectives(L0,L1) :-
-   adjectives(L0,L1).
+    adjectives(L0,L1).
+opt_adjectives(L,L).
 
-opt_pp(L,L).
 opt_pp(L0,L1) :-
-   pp(L0,L1).
+    pp(L0,L1).
+opt_pp(L,L).
 
-opt_noun_phrase(L,L).
 opt_noun_phrase(L0,L1) :-
-   noun_phrase(L0,L1).
+    noun_phrase(L0,L1).
+opt_noun_phrase(L,L).
+
+% ************************ Using the Conversation Ontology ************************
+
+% relation to access types of words words in our ontology
+property(Word, Property, Value) :-
+    word(Word, PropertyList),
+    member(Property = Value, PropertyList).
+
+% ************************ Dictionaries For Word Type ************************
+
+det([StringWord | L], L) :-
+  atom_string(Word, StringWord),
+  property(Word, type, det).
+det(L,L).
+
+noun([StringWord | L], L) :-
+  atom_string(Word, StringWord),
+  property(Word, type, noun).
+
+adj([StringWord | L],L) :-
+  atom_string(Word, StringWord),
+  property(Word, type, adj).
+
+verb([StringWord | L],L) :-
+  atom_string(Word, StringWord),
+  property(Word, type, verb).
+
+preposition([StringWord | L],L) :-
+  atom_string(Word, StringWord),
+  property(Word, type, prep).
